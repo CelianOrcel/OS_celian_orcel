04ab5daed8694615441a3d0029de9606feeeaa74
CelianOrcel <celian.orcel@iut-rodez.fr>
2025-10-10 09:45:10 +0000

    thread

---
+++ Parallelisation/Calcule.java
@@ -0,0 +1,40 @@
+public class Calcule extends Thread {
+    
+    // déclaration des variables d'instance
+    private int valeur;
+    private int multplicateur;
+    private int index;
+    private long resultat;
+
+    /**
+     * Constructeur de la classe Calcule
+     * @param valeur La valeur à traiter
+     * @param multplicateur Le multiplicateur pour le calcul intensif
+     * @param index L'index de la valeur dans le tableau d'origine
+     */
+    public Calcul (int valeur, int multplicateur, int index) {
+        this.valeur = valeur;
+        this.multplicateur = multplicateur;
+        this.index = index;
+        this.resultat = 0;
+    }
+
+    /**
+     * Méthode run exécutée lors du démarrage du thread
+     */
+    @Override
+    public void run() {
+        // Même logique que dans la boucle interne de CalculSequentiel
+        for (int j = 0; j < multplicateur; j++) {
+            resultat += valeur * valeur + valeur;
+        }
+    }
+
+    /**
+     * Méthode pour récupérer le résultat du calcul
+     * @return Le résultat du calcul
+     */
+    public long getResultat() {
+        return resultat;
+    }
+}
