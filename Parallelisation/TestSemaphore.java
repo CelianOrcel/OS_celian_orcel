5cd2bf7c7b1ea53b0dd5b2791694aa1f5c0c7d2a
CelianOrcel <celian.orcel@iut-rodez.fr>
2025-10-20 15:03:03 +0000

    testsemaphore

--- Parallelisation/TestSemaphore.java
+++ Parallelisation/TestSemaphore.java
@@ -1,6 +1,8 @@
 public class TestSemaphore {
 
     public static void main(String[] args) {
+        ProducteurConsomateur ProducteurConsomateur = new ProducteurConsomateur();
+
         System.out.println("=== Test Semaphore - Scénario équilibré ===");
 
         
